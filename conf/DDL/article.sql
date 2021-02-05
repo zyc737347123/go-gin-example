8ca0021a304260a73047612726b6b6d4f1a55b85
zyc737347123 <zyc737347123@163.com>
2021-02-05 22:56:09 +0800

    init db and table

---
+++ conf/DDL/article.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `blog_article` (
+                                `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                                `tag_id` int(10) unsigned DEFAULT '0' COMMENT '标签ID',
+                                `title` varchar(100) DEFAULT '' COMMENT '文章标题',
+                                `desc` varchar(255) DEFAULT '' COMMENT '简述',
+                                `content` text,
+                                `created_on` int(11) DEFAULT NULL,
+                                `created_by` varchar(100) DEFAULT '' COMMENT '创建人',
+                                `modified_on` int(10) unsigned DEFAULT '0' COMMENT '修改时间',
+                                `modified_by` varchar(255) DEFAULT '' COMMENT '修改人',
+                                `deleted_on` int(10) unsigned DEFAULT '0',
+                                `state` tinyint(3) unsigned DEFAULT '1' COMMENT '状态 0为禁用1为启用',
+                                PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文章管理';
---
+++ conf/DDL/auth.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `blog_auth` (
+                             `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                             `username` varchar(50) DEFAULT '' COMMENT '账号',
+                             `password` varchar(50) DEFAULT '' COMMENT '密码',
+                             PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+# INSERT INTO `blog`.`blog_auth` (`id`, `username`, `password`) VALUES (null, 'test', 'test123456');
