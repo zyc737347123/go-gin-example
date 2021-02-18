3cc500f94f9c73decdaa6aa30e028e08432c05cf
zyc737347123 <zyc737347123@gmail.com>
2021-02-18 15:51:46 +0800

    [feat] 增加article接口

--- go.mod
+++ go.mod
@@ -13,7 +13,9 @@ replace (
 )
 
 require (
+	github.com/EDDYCJY/go-gin-example v0.0.0-20201228125222-28f372bf41f9
 	github.com/astaxie/beego v1.12.3
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-ini/ini v1.62.0
 	github.com/jinzhu/gorm v1.9.16
---
+++ middleware/jwt/jwt.go
@@ -0,0 +1,46 @@
+package jwt
+
+import (
+	"go/token"
+	"time"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/zyc737347123/go-gin-example/pkg/util"
+	"github.com/zyc737347123/go-gin-example/pkg/e"
+)
+
+func JWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var code int
+		var data interface{}
+
+		code = e.SUCCESS
+		token := c.Query("token")
+		if token == "" {
+			code = e.INVALID_PARAMS
+		} else {
+			claims, err := util.ParseToken(token)
+			if err != nil {
+				code = e.ERROR_AUTH_CHECK_TOKEN_FAIL
+			} else if time.Now().Unix() > claims.ExpiresAt {
+				code = e.ERROR_AUTH_CHECK_TOKEN_TIMEOUT
+			}
+		}
+
+		if code != e.SUCCESS {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"code" : code,
+				"msg" : e.GetMsg(code),
+				"data" : data,
+			})
+
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
+
