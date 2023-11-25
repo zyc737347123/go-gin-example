a182c91bc4d4390fb56cb8771df37f243c5ef93c
zyc737347123 <zyc737347123@gmail.com>
2023-11-25 10:46:45 +0800

    init

--- main.go
+++ main.go
@@ -12,12 +12,13 @@ func main() {
 	router := routers.InitRouter()
 
 	s := &http.Server{
-		Addr: fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler: router,
-		ReadTimeout: setting.ReadTimeout,
-		WriteTimeout: setting.WriteTimeout,
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
 
 	s.ListenAndServe()
+	//
 }
